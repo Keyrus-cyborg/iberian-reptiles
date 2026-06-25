b2cdb3f5d8f270d5d0c6fcf413fdf3b5f7f53dc8
Keyrus-cyborg <antoniocm1991@gmail.com>
2026-06-25 14:31:52 +0200

    feat: se añaden los campos de SpeciesCard

--- src/components/SpeciesCard.js
+++ src/components/SpeciesCard.js
@@ -3,6 +3,9 @@ function SpeciesCard({ nombre, imagen }){
         <div className="card">
             <img src={imagen} alt={nombre} />
             <h3>{nombre}</h3>
+            <span className="scientific-name">{nombreCientifico}</span>
+            <span className="subtype">{subtipo}</span>
+            <span className={`status ${estadoConservacion}`}>{estadoConservacion}</span>
         </div>
     );
 }
