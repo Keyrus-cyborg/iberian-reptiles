dec86611b73311530e3d5e85600075e6e6c505a9
Keyrus-cyborg <antoniocm1991@gmail.com>
2026-07-03 16:54:02 +0200

    feat: definir CategoryPage para que se puede cambiar la vista al cambiar de sección

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,7 +1,48 @@
-function CategoryPage() {
+import { useState } from 'react'
+import { reptiles } from '../data/reptiles';
+import SearchBar from "../components/SearchBar";
+import HeroBanner from "../components/HeroBanner";
+import SpeciesCard from "../components/SpeciesCard";
+
+function CategoryPage({ categoria }) {
+    const [search, setSearch] = useState('');
+
+    const reptilesCategoria = reptiles.filter(
+        (reptil) => reptil.categoria === categoria
+    )
+
+    const reptilesFiltrados = reptilesCategoria.filter((reptil) => {
+        const texto = search.toLowerCase();
+
+        return (
+            reptil.nombre.toLowerCase().includes(texto) ||
+            reptil.nombreCientifico.toLowerCase().includes(texto) ||
+            reptil.subtipo.toLowerCase().includes(texto) ||
+            reptil.estadoConservacion.toLowerCase().includes(texto)
+        )
+    });
+
     return (
-        <h1>Category Page</h1>
-    );
+        <>
+            <div className="hero-banner-container">
+                <HeroBanner />
+            </div>
+            <div className="search-container">
+                <SearchBar
+                    search={search}
+                    setSearch={setSearch}
+                />
+            </div>
+            <div className="cards-container">
+                {reptilesFiltrados.map((reptil) => (
+                    <SpeciesCard
+                        key={reptil.id}
+                        reptil={reptil}
+                    />
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default CategoryPage;
