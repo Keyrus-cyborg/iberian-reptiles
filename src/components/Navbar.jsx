c0406f621f18d819a7b4b3ff9fee0a131fd7c049
Keyrus-cyborg <antoniocm1991@gmail.com>
2026-07-21 20:44:50 +0200

    style: añadir línea a sección actual de la página

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
@@ -8,11 +9,11 @@ function Navbar() {
                     <img src="/logos/logo-reptiles.png" className="logo-nav" alt="Logo Reptiles"></img>
                 </div>
                 <div className="menu">
-                    <Link to="/">Inicio</Link>
-                    <Link to="/lagartos">Lagartos</Link>
-                    <Link to="/serpientes">Serpientes</Link>
-                    <Link to="/quelonios">Quelonios</Link>
-                    <Link to="/geckos-camaleones">Geckos y camaleones</Link>
+                    <NavLink to="/">Inicio</NavLink>
+                    <NavLink to="/lagartos">Lagartos</NavLink>
+                    <NavLink to="/serpientes">Serpientes</NavLink>
+                    <NavLink to="/quelonios">Quelonios</NavLink>
+                    <NavLink to="/geckos-camaleones">Geckos y camaleones</NavLink>
                 </div>
             </div>
         </nav>
