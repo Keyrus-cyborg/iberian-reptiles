7a2866f855baf4806072cc8f070ccf0e46b52fc6
Keyrus-cyborg <antoniocm1991@gmail.com>
2026-07-22 19:25:49 +0200

    feat: añadir footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import './App.css'
 
 import { Routes, Route } from "react-router-dom";
@@ -44,6 +45,7 @@ function App() {
 
         </Routes>
       </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+            <img src="logos/logo-reptiles.png" alt="Logo Reptiles" className="footer-logo"></img>
+            <p>Información divulgativa sobre las especies de reptiles de la Península Ibérica.</p>
+            <p className="footer-copy">© 2026 · Proyecto desarrollado con React</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
